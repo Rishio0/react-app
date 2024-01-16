72ad4282d50adfa683b2518e707596da89c28797
Rishio0 <rishabhmeena917@gmail.com>
2024-01-16 07:04:14 +0000

    update code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World, Jenkins!
+          Hello World, Jenkins_v2!
         </p>
         <a
           className="App-link"
